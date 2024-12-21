498a268d41c94d291d44763412bd3d34a33b6074
scarlet <scarryaa@gmail.com>
2024-12-21 16:31:05 -0500

    adjusted windows window class registration

--- src/platform/windows.zig
+++ src/platform/windows.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const c = @cImport({
     @cDefine("WIN32_LEAN_AND_MEAN", "1");
+    @cDefine("UNICODE", "1");
     @cInclude("windows.h");
 });
 
@@ -8,24 +9,24 @@ pub const Window = struct {
     handle: ?c.HWND,
 
     pub fn init() !Window {
-        const instance = c.GetModuleHandleA(null);
-        const WHITE_BRUSH: c.HBRUSH = @ptrCast(c.GetStockObject(c.WHITE_BRUSH));
+        const instance = c.GetModuleHandleW(null);
+        const WHITE_BRUSH: c.HBRUSH = @ptrCast(@alignCast(c.GetStockObject(c.WHITE_BRUSH)));
 
         // Register window class
-        var window_class = std.mem.zeroes(c.WNDCLASSEXA);
-        window_class.cbSize = @sizeOf(c.WNDCLASSEXA);
+        var window_class = std.mem.zeroes(c.WNDCLASSEXW);
+        window_class.cbSize = @sizeOf(c.WNDCLASSEXW);
         window_class.hInstance = instance;
-        window_class.lpszClassName = "MyWindow";
+        window_class.lpszClassName = &[_:0]u16{ 'M', 'y', 'W', 'i', 'n', 'd', 'o', 'w' };
         window_class.lpfnWndProc = wndProc;
         window_class.style = c.CS_HREDRAW | c.CS_VREDRAW;
-        window_class.hCursor = c.LoadCursorA(null, c.IDC_ARROW);
+        window_class.hCursor = c.LoadCursorW(null, c.IDC_ARROW);
         window_class.hbrBackground = WHITE_BRUSH;
 
-        if (c.RegisterClassExA(&window_class) == 0) {
+        if (c.RegisterClassExW(&window_class) == 0) {
             return error.RegisterClassFailed;
         }
 
-        const window = c.CreateWindowExA(0, "MyWindow", "Window Title", c.WS_OVERLAPPEDWINDOW, c.CW_USEDEFAULT, c.CW_USEDEFAULT, 800, 600, null, null, instance, null);
+        const window = c.CreateWindowExW(0, window_class.lpszClassName, &[_:0]u16{ 'W', 'i', 'n', 'd', 'o', 'w', ' ', 'T', 'i', 't', 'l', 'e' }, c.WS_OVERLAPPEDWINDOW, c.CW_USEDEFAULT, c.CW_USEDEFAULT, 800, 600, null, null, instance, null);
 
         if (window == null) {
             return error.CreateWindowFailed;
@@ -41,13 +42,13 @@ pub const Window = struct {
 };
 
 fn wndProc(hwnd: c.HWND, msg: c.UINT, wp: c.WPARAM, lp: c.LPARAM) callconv(.C) c.LRESULT {
-    return c.DefWindowProcA(hwnd, msg, wp, lp);
+    return c.DefWindowProcW(hwnd, msg, wp, lp);
 }
 
 pub fn runEventLoop() !void {
     var msg: c.MSG = undefined;
-    while (c.GetMessageA(&msg, null, 0, 0) > 0) {
+    while (c.GetMessageW(&msg, null, 0, 0) > 0) {
         _ = c.TranslateMessage(&msg);
-        _ = c.DispatchMessageA(&msg);
+        _ = c.DispatchMessageW(&msg);
     }
 }
